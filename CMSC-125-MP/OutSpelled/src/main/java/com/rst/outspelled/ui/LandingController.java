1a31dc08d29c9145aa9b700084382e3f37853b35
ftsaturay-bit <ftsaturay@up.edu.ph>
2026-05-22 14:27:16 +0800

    Feat: Changed the background of profile-view to dungeon-brick.png wip: gradient for transition, player cards and buttons

--- CMSC-125-MP/OutSpelled/src/main/java/com/rst/outspelled/ui/LandingController.java
+++ CMSC-125-MP/OutSpelled/src/main/java/com/rst/outspelled/ui/LandingController.java
@@ -125,7 +125,7 @@ public class LandingController {
             Circle star = new Circle(radius, Color.web("#fffdd0", 0.75));
             //randomize x and y positions
             star.layoutXProperty().bind(rootPane.widthProperty().multiply(Math.random()));
-            //limit the stars to top 44% of the screen
+            //limit the stars to the top 44% of the screen
             star.layoutYProperty().bind(rootPane.heightProperty().multiply(Math.random() * 0.44)); 
             weatherPane.getChildren().add(star);
             starCluster.add(star);
